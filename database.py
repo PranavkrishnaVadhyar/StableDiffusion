c407b077915fee05e3de42bec07247d5d7cb4a31
B Pranavkrishna Vadhyar <96326149+PranavkrishnaVadhyar@users.noreply.github.com>
2023-08-06 16:27:05 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,11 +4,10 @@ from firebase_admin import db
 
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('test-f73e3-firebase-adminsdk-18gr7-8af7db525b.json')
-
+cred = credentials.Certificate('')
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://test-f73e3-default-rtdb.asia-southeast1.firebasedatabase.app'
+    'databaseURL': ''
 })
 
 # As an admin, the app has access to read and write all data, regradless of Security Rules
